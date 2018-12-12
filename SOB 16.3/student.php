fff529eb65621d5d4163608cd7668a68e95d074a
Deepam Gupta <deepam.deepam.gupta@gmail.com>
2018-12-12 20:25:20 +0530

    php version, chidiya resolved, developers resolved

---
+++ SOB 16.3/student.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- head -->
+<?php include 'includes/head.php';?>
+
+<body>
+
+
+	<!-- Header -->
+	<?php include 'includes/header.php';?>
+
+	<!-- Navigation Bar -->
+	<?php include 'includes/navbar.php'?>
+
+	<!-- Side Nav -->
+	<?php include 'includes/sidenav.php';?>
+
+	<!-------------- STUDENT (Start) ------------------->
+
+	<div class="student" id="student">
+
+	 		<!-- <div>
+	 			<h1 class="display-3" style="text-align: center; padding-top: 7%;">STUDENTS</h1>
+	 		</div> -->
+	 		<div class="container" style="background-color: inherit;">
+	 			<div class="row">
+	 				<div class="col-md-12 subhead">
+	 					<h1 class="display-4"><strong>STUDENT</strong></h1>
+	 				</div>
+	 			</div>
+	 		</div>
+
+
+	 		<div class="container" style="margin-top: 2%;">
+	 			<div class="row jumbotron">
+
+	 				<div id="h" class="col-md-6 border1" id="student-calender">
+	 					<div>
+	 						<div id="acalender">
+	 							<h2  style="font-weight: 300; text-align:center; margin-bottom:0px;">Academic Calender <i class="fa fa-calendar" aria-hidden="true"></i></h2>
+	 						</div>
+	 						<br>
+	 						<br>
+	 						<div style="float: none;">
+	 							<span> <a class="link btn btn-success  btn-responsive" href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download>Download PDF</a></span>
+	 						</div>
+	 					</div>
+	 				</div>
+
+	 				<div class="col-md-6" id="student-syllabus">
+	 					<div style="float: right;">
+	 						<div>
+	 							<h2  id="cms" style="font-weight: 300; text-align:center; margin-bottom:0px;">Course Material & Syllabus <i class="fa fa-graduation-cap" aria-hidden="true"></i></h2>
+	 						</div>
+	 						<br>
+	 						<br>
+	 						<div class="row">
+	 							<div class="col-md-6">
+	 								<span style="float: left;">
+	 									<h4>M.Sc.&nbsp;&nbsp;&nbsp;&nbsp;<span> <a href="docs/MScSyllabus2017-19.PDF" class="btn btn-success btn-sm" target="_blank" download>PDF</a></span></h4>
+	 								</span>
+	 							</div>
+
+	 							<div class="col-md-6">
+	 								<span style="float: left;">
+	 									<h4>Ph.D.&nbsp;&nbsp;&nbsp;&nbsp;<span> <a href="#" class="btn btn-success btn-sm" target="_blank" download>PDF</a></span></h4>
+	 								</span>
+	 							</div>
+	 						</div>
+	 					</div>
+	 				</div>
+
+
+	 				<div class="row jumbotron" style="margin-top: 4%;" id="student-timetable">
+	 					<div  class="normal col-md-12 subhead-line1" id="ttable">
+	 						<h2 style="font-weight: 500">Time Table <i class="fa fa-calendar" aria-hidden="true"></i></h2>
+	 					</div>
+
+	 					<div class="col-md-6">
+
+	 						<div class="card">
+	 							<div class="card-body">
+	 								<h4 class="card-title">M.Sc.</h4>
+	 								<table class="table table-hover">
+	 									<thead>
+	 										<tr>
+	 											<th>Semester</th>
+	 											<th></th>
+	 										</tr>
+	 									</thead>
+	 									<tbody>
+	 										<tr>
+	 											<td>I (July – Dec)</td>
+	 											<td><a href="docs/semIbio.PDF" target="_blank" download>Download PDF</a></td>
+	 										</tr>
+	 										<tr>
+	 											<td>II (Jan – May)</td>
+	 											<td><a href="docs/Sem-IITimeTable2018Toupload12042018.PDF" target="_blank" download>Download PDF</a></td>
+	 										</tr>
+	 										<tr>
+	 											<td>III (July – Dec)</td>
+	 											<td><a href="docs/semIIIbio.pdf" target="_blank" download>Download PDF</a></td>
+	 										</tr>
+	 									</tbody>
+	 								</table>
+
+	 							</div>
+	 						</div>
+	 					</div>
+
+	 					<div class="col-md-6">
+
+	 						<div class="card">
+	 							<div class="card-body">
+	 								<h4 class="card-title">Ph.D.</h4>
+	 								<table class="table table-hover">
+	 									<thead>
+	 										<tr>
+	 											<th>Course work</th>
+	 											<th>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </th>
+	 										</tr>
+	 									</thead>
+	 									<tbody>
+	 										<tr>
+	 											<td><a href="docs/Ph.DCourseWorkTimeTable12042018.PDF" target="_blank" download>Download PDF</a></td>
+	 											<td>&nbsp &nbsp</td>
+	 										</tr>
+	 										<tr>
+	 											<td>&nbsp &nbsp</td>
+	 											<td>&nbsp &nbsp</td>
+	 										</tr>
+	 										<tr>
+	 											<td> &nbsp &nbsp</td>
+	 											<td>&nbsp &nbsp</td>
+	 										</tr>
+	 									</tbody>
+	 								</table>
+	 							</div>
+	 						</div>
+	 					</div>
+	 				</div>
+
+	 				<div class="row jumbotron" style="margin-top: -5%;">
+	 					<div class="normal col-md-12 subhead-line1">
+	 						<h2 style="font-weight: 500">Exam Schedule <i class="fa fa-hourglass-half" aria-hidden="true"></i></h2>
+	 					</div>
+
+	 					<div class="col-md-6">
+
+	 						<div class="card">
+	 							<div class="card-body">
+	 								<h4 class="card-title">M.Sc.</h4>
+	 								<table class="table table-hover">
+	 									<thead>
+	 										<tr>
+	 											<th>Test</th>
+	 											<th>Semester I & III</th>
+	 											<th>Semester II</th>
+
+	 										</tr>
+	 									</thead>
+	 									<tbody>
+	 										<tr>
+	 											<td>Unit test I</td>
+	 											<td><a href="docs/TestsIJuly-Dec2018.PDF" target="_blank" download>Download PDF</a></td>
+	 											<td><a href="docs/Sem-IIUnitTest ITimeTableToupload12042018.PDF" target="_blank" download>Download PDF</a></td>
+	 										</tr>
+	 										<tr>
+	 											<td>Unit test II</td>
+	 											<td><a href="docs/TestIIJuly-Dec2018.PDF" target="_blank" download>Download PDF</a></td>
+	 											<td><a href="docs/Sem-II Unit Test III Time Table To upload.PDF" target="_blank" download>Download PDF</a></td>
+	 										</tr>
+	 										<tr>
+	 											<td>Unit test III </td>
+	 											<td><a href="docs/TestsIII,July-Dec 2018.PDF" target="_blank" download>Download PDF</a></td>
+	 											<td><a href="docs/Sem-II Unit Test III Time Table To upload.PDF" target="_blank" download> Download PDF</a></td>
+	 										</tr>
+	 										<tr>
+	 											<td>Final Sem exam</td>
+	 											<td><a href="docs/Sem Exam Dec 2018.PDF" target="_blank" download>Download PDF</a></td>
+	 											<td><a href="docs/Sem-II Final Exam Time Table To upload.PDF" target="_blank" download>Download PDF</a></td>
+	 										</tr>
+	 									</tbody>
+	 								</table>
+
+	 							</div>
+	 						</div>
+	 					</div>
+
+	 					<div class="col-md-6">
+
+	 						<div class="card">
+	 							<div class="card-body">
+	 								<h4 class="card-title">Ph.D. (course work)</h4>
+	 								<table class="table table-hover">
+
+	 									<tbody>
+	 										<tr>
+	 											<td><a href="docs/Ph.D20042018.PDF" target="_blank" download>Download PDF</a></td>
+	 										</tr>
+	 										<tr>
+	 											<td>&nbsp &nbsp &nbsp &nbsp</td>
+	 										</tr>
+	 										<tr>
+	 											<td>&nbsp &nbsp &nbsp &nbsp</td>
+	 										</tr>
+	 										<tr>
+	 											<td>&nbsp &nbsp &nbsp &nbsp</td>
+	 										</tr>
+	 										<tr>
+	 											<td>&nbsp &nbsp &nbsp &nbsp</td>
+	 										</tr>
+	 									</tbody>
+	 								</table>
+	 							</div>
+	 						</div>
+	 					</div>
+	 				</div>
+	 			</div>
+	 		</div>
+	 	</div>
+
+	 	<!-------------- STUDENT(End) ------------------->
+
+	 	<!-- Up Arrow -->
+	 	<?php include 'includes/uparrow.php';?>
+
+	 	<!-- footer -->
+	 	<?php include 'includes/footer.php';?>
+
+	 </body>
+	 </html>
