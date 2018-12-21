eb78b9c65d2b3bcafd772ed414f94365f72d919b
Deepam Gupta <deepam.deepam.gupta@gmail.com>
2018-12-22 01:07:32 +0530

    updated faculty publications

--- fac1.php
+++ fac1.php
@@ -102,69 +102,10 @@
 				</section>
 				<section>
 					<div class="fac-subhead">
-						<h2>PUBLICATIONS</h2>
+						<h2>PUBLICATIONS&nbsp &nbsp &nbsp  </h2>
 					</div>
 					<div class="fac-content" id="publications">
-						<p>List of Publications :-</p>
-						<ol>
-							<li>The Red Cell Membrane Structure : Some features of interest to Hematologists. N.G.Mehta, S.M.Gokhale and R.D.Kalraiya Indian J. Hematol. Vol. 3, 41-48, (1985). ISSN – 0971-4502</li>
-							<li>Glycophorin A interferes in the agglutination of human erythrocytes by concanavalin A. Explanation for the requirement of enzymatic predigestion. S.M.Gokhale and N.G.Mehta Biochem. J.(U.K.), 241, 505-511, (1987). ISSN – 0264-6021</li>
-							<li>Concanavalin A-agglutinability of membrane-skeleton-free vesicles and aged cellular remnants derived from human erythrocytes. Is the membrane skeleton required for aggiutination ? S.M.Gokhale and N.G.Mehta Biochem. J.(U.K.), 241, 513-520, (1987). ISSN – 0264-6021</li>
-							<li>Concanavalin A binding to human erythrocytes leads to alterations in properties of the membrane skeleton. S.M.Gokhale and N.G.Mehta Biochem. J.(U.K.), 241, 521-525, (1987). ISSN – 0264-6021</li>
-							<li>Endogenous factors determining the agglutinability of erythrocytes with concanavalin A. N.G.Mehta, S.M.Gokhale and K.N.Pestonjamasp Indian J. Biochem. Biophys. 25, 14-19, (1988). ISSN – 0301-1208</li>
-							<li>Membrane skeleton and transbilayer phospholipid organization in heat-treated human erythrocytes. S.R.P.Gudi, A.Kumar, V.Bhakuni, S.M.Gokhale and C.M.Gupta     Biochim. Biophys. Acta 1023, 63-72, (1990). ISSN – 0005-2736</li>
-							<li>Heat-induced alterations in monkey erythrocyte membrane phospholipid organization and skeletal protein structure and interactions. A.Kumar, S.R.P.Gudi, S.M.Gokhale,  V.Bhakuni and C.M.Gupta Biochim. Biophys. Acta 1030, 269-278, (1990). ISSN – 0005-2736</li>
-							<li>The role of the membrane skeleton in concanavalin A-mediated agglutination of human erythrocytes. K.N. Pestonjamasp, S.M. Gokhale and N.G.Mehta Biotechnol Appl Biochem.12 (5), 544-549, (1990) ISSN – 0885-4513</li>
-							<li>Solubility behavior of integral proteins and glycophorins of mammalian erythrocyte membrane. Savita Sharma and S. M. Gokhale Asian J. Exp. Biol. Sci. Vol. 2 (3), 449-454 (2011) ISSN - 0975-5845</li>
-							<li>Sialoglycoproteins of mammalian erythrocyte membranes: A comparative study. Savita Sharma and Sadashiv M. Gokhale Asian-Australian J. Anim. Sci. Vol. 24 (12), 1666-1673 (2011) Online ISSN - 1976-5517            Print ISSN - 1011-2367 </li>
-							<li>Differential actions of proteinases and neuraminidase on mammalian erythrocyte surface and its impact on erythrocyte agglutination by concanavalin A. Gen. Physiol. Biophys. 31, 457–468 (2012)  ISSN - 0231-5882</li>
-							<li>Identification of human erythrocyte cytosolic proteins associated with plasma membrane during thermal stress. Savita Sharma, Surekha M. Zingde and Sadashiv M. Gokhale   J. Membrane Biology 246 (8), 591-607 (2013) Online ISSN- 1432-1424            Print ISSN- 0022-2631</li>
-							<li>Marker assisted backcross selection for genetic removal of lipoxygenase-2
-								from popular soybean (Glycine max) variety JS 97-52: Parental polymorphism
-								survey and hybridity validation.
-								Reena Rawal, Vineet Kumar, Anita Rani, S M Gokhale and S M Husain
-								Indian Journal of Agricultural Sciences 84 (3): 414–7, March 2014
-								ISSN: 0019-5022
-							Impact factor- 0.141 (2014), 0.231 (2017/18)</li>
-							<li>A comparative study of the membrane proteins of some mammalian
-								erythrocytes identified by mass spectrometry.
-								Savita Sharma, Vinny Punjabi, Surekha M. Zingde and Sadashiv M. Gokhale
-								J. Membrane Biology 247(11),1181–1189, Oct 2014
-								Online ISSN: 1432-1424	Print ISSN: 0022-2631 (Springer)
-							Impact factor- 2.457 (2014), 1.638 (2017/18)</li>
-							<li>Enzymatic cleavage of cell surface proteins of pig and cow erythrocytes and its
-								effect on concanavalin-mediated agglutinability.
-								Savita Sharma and Sadashiv M. Gokhale
-								Indian J Biochem Biophys 51(5) 378-387,Oct 2014
-								Online ISSN: 0975-0959	Print ISSN: 0301-1208
-							Impact factor- 0.871 (2014), 0.385 (2017/18) </li>
-							<li>Alkyloxy carbonyl modified hexapeptides as a high affinity compounds
-								for Wnt5A protein in the treatment of psoriasis.
-								Ankit Kelotra, Sadashiv M Gokhale, Seema Kelotra, Vaidehi Mukadam,
-								Komal Nagwanshi, Srinivas Bandaru, Anuraj Nayarisseri & Anil Bidwai
-								Bioinformation 10(12): 739-745, Dec 2014
-							Online ISSN: 0973-2063  Print ISSN: 0973-8894 </li>
-							<li>Morphological trait variations in the west Himalayan (India) populations of
-								Arabidopsis thaliana along altitudinal gradients.
-								Akanksha Singh, Antariksh Tyagi, Abhinandan Mani Tripathi, Sadashiv
-								M. Gokhale, Nandita Singh and Sribash Roy.
-								Current Science 108 (12): 2213-2222, June 2015
-								Online ISSN: 0011-3891
-							Impact factor- 0.967 (2015), 0.883 (2017/18) </li>
-							<li>Effect of free radical generating system- Adenosine Triphosphate/Ferrous
-								Sulphate/Hydrogen Peroxide on human erythrocytes.
-								Vinny Punjabi and Sadashiv M Gokhale
-								Inter. J. of Contemporary Res. and Rev. 8 (7), 20184-20192, July 2017
-							Online ISSN: 0976 – 4852</li>
-							<li>Structure of Asp-bound peptidase E from Salmonella enterica: Active site at
-								dimer interface illuminates Asp recognition.
-								Pooja Yadav, Venuka Durani Goyal, Neeraj K. Gaur, Ashwani Kumar,
-								Sadashiv M. Gokhale and Ravindra D. Makde
-								FEBS Letters- doi:10.1002/1873-3468.13247, Sept 2018
-								Online ISSN: 1873-3468	Print ISSN: 0014-5793
-							Impact factor: 2.999 (2017/18)</li>
-
-						</ol>
+						<p>Download list of publications from here : &nbsp&nbsp<a href="docs/faculty/Publications_SM_Gokhale.pdf" class="btn btn-info btn-sm" download="Dr.SM_Gokhale_Publicationss">DOWNLOAD</a></p>
 					</div>
 				</section>
 			</div>
