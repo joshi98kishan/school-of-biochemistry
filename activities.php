a54e83f0e4034a77470366257a32d796b07e19fa
Rajat-Karahe <29karaherajat@gmail.com>
2018-12-22 00:48:17 +0530

    Add files via upload

---
+++ activities.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- head -->
+<?php include 'includes/head.php';?>
+
+<body>
+
+	<!-- Header -->
+	<?php include 'includes/header.php';?>
+
+	<!-- Navigation Bar -->
+	<?php include 'includes/navbar.php'?>
+	<!-- Side Nav -->
+	<?php include 'includes/sidenav.php';?>
+
+
+	<!------------------------------------------- ACTIVITIES(Start) -------------------------------------------->
+
+	
+	
+	<div id="Activities" class="container-fluid">
+		<div class="container"><h1 class="display-4">ACTIVITIES</h1></div>
+		<div class="container cont-marg" id="cont-margs">
+			<hr class="horizontal-act">
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download><div class="display-4 text-block"><h3>16/05/2018</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">A talk on “Cellular Senescence and its role in aging and disease”   
+						<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download>click here for more</a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download><div class="display-4 text-block"><h3>19/04/2018</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">Three day Workshop on “Practical Experiences of Meditation” 
+						<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download>click here for more</a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download><div class="display-4 text-block"><h3>22/03/2018</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">Health cards for Students of the department 
+						<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download>click here for more</a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="#"><div class="display-4 text-block"><h3>14/03/2018</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">Mr Venkat Narayan Are participated in Final round of Anvention 2017  
+						<a href="#">click here for more</a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download><div class="display-4 text-block"><h3>18/01/2018</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">M.Sc. Semester II students attended Training Programme  
+						<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download>click here for more</a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download><div class="display-4 text-block"><h3>10/01/2018</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">Dr. Meeta Jain attended World Environment Congress  
+						<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download>click here for more</a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download><div class="display-4 text-block"><h3>05/01/2018</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">Dr Sunita Shukla participated in International Symposium 
+						<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download>click here for more</a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="#"><div class="display-4 text-block"><h3>02/12/2017</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">Dr Sunita Kataria participated in International Conference 
+						<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download>click here for more</a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download><div class="display-4 text-block"><h3>27/11/2017</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">Dr Sunita Shukla participated in National Workshop  
+						<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download>click here for more</a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download><div class="display-4 text-block"><h3>22/11/2017</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">Mr Venkat Narayan Are participated in Central Zone Convention of Anvention 2017  
+						<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download>click here for more</a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download><div class="display-4 text-block"><h3>06/11/2017</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">Mr Venkat Narayan Are presented a Poster and Paper of Anvention 2017  
+						<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download>click here for more</a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download><div class="display-4 text-block"><h3>23/10/2017</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">Students’ Participation in GIAN  
+						<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download>click here for more</a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download><div class="display-4 text-block"><h3>23/10/2017</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">Students’ Election Activities  
+						<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download>click here for more</a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="#"><div class="display-4 text-block"><h3>05/09/2017</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">Teachers Day Celebration  
+						<a href="#">click here for more</a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download><div class="display-4 text-block"><h3>24/08/2017</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">Registration of students on NCS portal  
+						<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download>click here for more</a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="no-jumbo row text-center">
+				<div class="image-item image-act">	
+					<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download><div class="display-4 text-block"><h3>10/07/2017</h3></div></a>
+				</div>
+
+				<div class="text-item">
+
+					<div class="actpelement">Guru Poornima Mahotsav  
+						<a href="docs/Academic Calender 2018 - 19.PDF" target="_blank" download>click here for more</a>
+					</div>
+
+				</div>
+			</div>
+			
+
+		</div>
+
+	</div>
+	
+
+
+
+	<!-- ---------------------ACTIVITIES END(End) ---------------------------->
+
+	<!-- Up Arrow -->
+	<?php include 'includes/uparrow.php';?>
+
+	<!-- footer -->
+	<?php include 'includes/footer.php';?>
+
+</body>
+</html>
