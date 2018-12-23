8318414bd9df333a30da514638c2672c61fb70e2
Samyak <samyak915jain@gmail.com>
2018-12-23 13:18:01 +0530

    navigation updated

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,7 +4,7 @@
 
 <hr class="abovenav">
 <div class="navicustom sticky-top">
-	<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+	<nav class="navbar navbar-expand-md navbar-light  sticky-top">
 		<div class="container-fluid" id="inner">
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
 				<span class="navbar-toggler-icon"></span>
@@ -30,7 +30,12 @@
 					</li>
 					<li class="nav-item"><a class="nav-link r p" href="researches.php"><strong>RESEARCHERS</strong></a></li>
 					<li class="nav-item"><a class="nav-link r p" href="activities.php"><strong>ACTIVITIES</strong></a></li>
-
+					<li class="nav-item dropdown"><a class="nav-link r p navw3" ><strong>Alumni &nbsp;</strong><i class="fas fa-caret-down"></i></a>
+						<div class="dropdown-menu ">
+							<a href="msc.php" class="linkst"><div class="dropdown-item" id="dr">M.Sc.</div></a>
+							<a href="phd.php" class="linkst"><div class="dropdown-item" id="dr">Ph.D.</div></a>
+						</div>
+					</li>
 					
 					<li class="nav-item"><a class="nav-link r p" target="_blank" href="http://www.dauniv.ac.in/"><strong>DAVV</strong></a></li>
 				</ul>
