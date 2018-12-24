62b81b195cbc3cd09e07f09c6060fe4c0dad7a49
Kishan Joshi <43925446+joshi98kishan@users.noreply.github.com>
2018-12-24 11:40:08 +0530

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -32,7 +32,7 @@
 					<li class="nav-item"><a class="nav-link r p" href="activities.php"><strong>ACTIVITIES</strong></a></li>
 					<li class="nav-item dropdown"><a class="nav-link r p navw3" ><strong>ALUMNI &nbsp;</strong><i class="fas fa-caret-down"></i></a>
 						<div class="dropdown-menu ">
-							<a href="msc.php" class="linkst"><div class="dropdown-item" id="dr">M.Sc.</div></a>
+							<a href="alumni-msc.php" class="linkst"><div class="dropdown-item" id="dr">M.Sc.</div></a>
 							<a href="alumni-phd.php" class="linkst"><div class="dropdown-item" id="dr">Ph.D.</div></a>
 						</div>
 					</li>
